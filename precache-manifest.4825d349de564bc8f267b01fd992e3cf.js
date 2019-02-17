dda29c5789f53705a2196103c6df3156c3b29e04
ullibodnar <ullikins@gmail.com>
2019-02-17 16:35:08 +1300

    Updates

--- precache-manifest.4825d349de564bc8f267b01fd992e3cf.js
+++ precache-manifest.4825d349de564bc8f267b01fd992e3cf.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/are-we-there-yet/static/js/runtime~main.c12427c5.js"
   },
   {
-    "revision": "0615a2b42113d4789f1e",
-    "url": "/are-we-there-yet/static/js/main.0615a2b4.chunk.js"
+    "revision": "e8ddf7e38d6af1a23784",
+    "url": "/are-we-there-yet/static/js/main.e8ddf7e3.chunk.js"
   },
   {
     "revision": "7130702acba81e094aa8",
     "url": "/are-we-there-yet/static/js/2.7130702a.chunk.js"
   },
   {
-    "revision": "0615a2b42113d4789f1e",
+    "revision": "e8ddf7e38d6af1a23784",
     "url": "/are-we-there-yet/static/css/main.104bd7d2.chunk.css"
   },
   {
-    "revision": "e8541b2c86a4e7069b3d9300ed1f47b7",
+    "revision": "e2fa285a4991ef4b42dd92a1d25ca071",
     "url": "/are-we-there-yet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/are-we-there-yet/precache-manifest.1b2f14ad83583220ec5e35df494a4687.js"
+  "/are-we-there-yet/precache-manifest.4825d349de564bc8f267b01fd992e3cf.js"
 );
 
 workbox.clientsClaim();
