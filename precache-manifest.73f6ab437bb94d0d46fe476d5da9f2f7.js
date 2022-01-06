4148a359cf8e705269e1274071686bd3bfa9425b
ullibodnar <ullikins@gmail.com>
2022-01-05 21:03:02 -0500

    Updates

---
+++ precache-manifest.73f6ab437bb94d0d46fe476d5da9f2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7312c6a3dd7d4e3bf1db",
+    "url": "/are-we-there-yet/static/css/main.8114cbf7.chunk.css"
+  },
+  {
+    "revision": "7312c6a3dd7d4e3bf1db",
+    "url": "/are-we-there-yet/static/js/main.7312c6a3.chunk.js"
+  },
+  {
+    "revision": "c12427c5f7596bfeaede",
+    "url": "/are-we-there-yet/static/js/runtime~main.c12427c5.js"
+  },
+  {
+    "revision": "8a6cb479d7c14bae24aa",
+    "url": "/are-we-there-yet/static/css/2.146827ac.chunk.css"
+  },
+  {
+    "revision": "8a6cb479d7c14bae24aa",
+    "url": "/are-we-there-yet/static/js/2.8a6cb479.chunk.js"
+  },
+  {
+    "revision": "cac46d7bbd6db930b7a4a8ba836b7568",
+    "url": "/are-we-there-yet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/are-we-there-yet/precache-manifest.c8bdb60b0419854258e383e91c7b8b23.js"
+  "/are-we-there-yet/precache-manifest.73f6ab437bb94d0d46fe476d5da9f2f7.js"
 );
 
 workbox.clientsClaim();
